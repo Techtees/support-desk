e382d6c9c218e2852bee6f22600aa0b9be67738f
taiwoj121 <taiwoj121@gmail.com>
2024-01-31 15:30:31 +0100

    async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,30 @@
+const asyncHandler = require('express-async-handler')
 
 // Register a new user
 //Route /api/users
 //access public
-const registerUser = (req, res) => {
-    const {name, email, password} = req.body
-   
-
-    //validation
-    if(!name || !email || !password) {
-        res.status(400)
-        throw new Error('please include all fields')
+const registerUser = asyncHandler(
+   async (req, res) => {
+        const {name, email, password} = req.body
+       
+    
+        //validation
+        if(!name || !email || !password) {
+            res.status(400)
+            throw new Error('please include all fields')
+        }
+        res.send('Register Route')
     }
-    res.send('Register Route')
-}
+) 
 
 // Login a new user
 //Route /api/users/login
 //access public
-const loginUser = (req, res) => {
-    res.send('Login Route')
-}
+const loginUser = asyncHandler(
+    async(req, res) => {
+        res.send('Login Route')
+    }
+) 
 
 module.exports = {
     registerUser,
