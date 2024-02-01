93bb7b8c2a908a461d43150dc5446d53d17d6572
taiwoj121 <taiwoj121@gmail.com>
2024-02-01 12:42:06 +0100

    user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
+
 
 // Register a new user
 //Route /api/users
@@ -13,7 +16,40 @@ const registerUser = asyncHandler(
             res.status(400)
             throw new Error('please include all fields')
         }
-        res.send('Register Route')
+
+        //Find if user already exists
+        const userExists = await User.findOne({email})
+
+        if(userExists) {
+            res.status(400)
+            throw new Error ('User already exists')
+        }
+
+        //hash password
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        //creae user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        if(user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                password: user.password
+            })
+        } else {
+            res.status(400)
+            throw new error('Invalid user data')
+        }
+
+       
     }
 ) 
 
