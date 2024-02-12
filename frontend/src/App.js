00f356cdf6e763ea621978ad6c10385410fa73fe
taiwoj121 <taiwoj121@gmail.com>
2024-02-12 13:18:43 +0100

    implemented private route for creating ticket on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Header from './component/Header';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './component/PrivateRoute';
 
 
 
@@ -18,6 +20,9 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/new-ticket' element={<PrivateRoute />} >
+            <Route path='/new-ticket' element={<NewTicket />} />
+          </Route>
         </Routes>
       </div>
     </Router>
