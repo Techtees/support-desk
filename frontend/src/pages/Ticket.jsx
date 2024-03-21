a5e456f45719d8905b8259ff43f5934394d38b72
taiwoj121 <taiwoj121@gmail.com>
2024-03-21 12:58:57 +0100

    display notes from backend

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -5,12 +5,13 @@ import {getNotes, reset as notesReset} from '../features/notes/noteSlice'
 import { useParams, useNavigate } from 'react-router-dom';
 import {BackButton} from '../component/BackButton'
 import Spinner from '../component/Spinner'; 
+import NoteItem from '../component/NoteItem';
 import {toast} from 'react-toastify'
 
 function Ticket() {
     const {ticket, isLoading, isSuccess, isError, message} = useSelector((state) => state.tickets)
 
-    const {note, isLoading:notesIsLoading } = useSelector((state) => state.notes)
+    const {notes, isLoading:notesIsLoading } = useSelector((state) => state.notes)
     // console.log(note)
     const params = useParams()
     const navigate = useNavigate()
@@ -35,7 +36,7 @@ function Ticket() {
 
     }
 
-    if(isLoading) {
+    if(isLoading || notesIsLoading) {
         return <Spinner />
     }
 
@@ -57,7 +58,11 @@ function Ticket() {
                     <h3>Description Issue</h3>
                     <p>{ticket.description}</p>
                 </div>
+                <h2>Notes</h2>
            </header>
+           {notes.map((note) => (
+            <NoteItem key={note._id} note ={note}  />
+           ))}
            {ticket.status !== 'closed' && (
             <button className='btn btn-block btn-danger' onClick={handleTicket}> Close TIcket</button>
            )}
